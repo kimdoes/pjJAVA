9c8c703037bbf288905491cfd29349c2558c0a80
kimdoes <kimdoes2143@naver.com>
2025-03-28 16:14:43 +0900

    DooBeeDooBeeDo System.out.print

--- practice/practice1.java
+++ practice/practice1.java
@@ -2,12 +2,13 @@ package practice;
 
 public class practice1 {
     public static void main (String[] args){
-        for (int i=2; i<10; i++){
-            System.out.print(i + "단\n");
-            for (int j=1; j<10; j++){
-                System.out.print(i + "*" + j + "=" + (i*j)+"\n");
+        for(int i=1; i<5; i++){
+            if (i%2==1){
+                System.out.print("Doo");
+            } else {
+                System.out.print(("Bee"));
             }
-            System.out.print("\n");
-    }  
+        }
+        System.out.print("Do");
     }
 }
