741c47a7895a392047629d85563015bf83f3cecb
kimdoes <kimdoes2143@naver.com>
2025-03-28 17:32:53 +0900

    자바 연습 수정본

--- practice/practice1.java
+++ practice/practice1.java
@@ -2,13 +2,21 @@ package practice;
 
 public class practice1 {
     public static void main (String[] args){
-        for(int i=1; i<5; i++){
-            if (i%2==1){
-                System.out.print("Doo");
-            } else {
-                System.out.print(("Bee"));
+        int bottleNum = 10;
+        String words = "Bottles";
+
+        while (bottleNum > 0){
+            if (bottleNum == 1){
+                words = "Bottle";
             }
+
+            System.out.println(bottleNum + " green " + words + " hanging on th wall");
+            System.out.println(bottleNum + " green " + words + " hanging on th wall");
+            System.out.println("And if one green bottle should accidentally fall");
+
+            bottleNum = bottleNum - 1;
         }
-        System.out.print("Do");
+
+        System.out.println("There'll be no green bottles hanging on the wall");
     }
 }
