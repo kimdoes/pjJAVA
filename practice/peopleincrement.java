382bb6a0109fa2cf980a28b7b1af854a3d1779a2
kimdoes <kimdoes2143@naver.com>
2025-05-07 14:41:57 +0900

    p.107 수영장퍼즐

--- practice/peopleincrement.java
+++ practice/peopleincrement.java
@@ -1,27 +1,34 @@
 package practice;
 
-class TestArrays {
-    public static void main (String[] args) {
-        int y = 0;
-        String [] islands = new String[4];
+class Triangle{
+    double area;
+    int height;
+    int length;
 
-        islands[0] = "Bermuda";
-        islands[1] = "Fiji";
-        islands[2] = "Azores";
-        islands[3] = "Cozumel";
+    public static void main(String[] args) {
+        int x = 0;
+        Triangle [] ta = new Triangle[4];
 
-        int [] index = new int[4];
-        index[0] = 1;
-        index[1] = 3;
-        index[2] = 0;
-        index[3] = 2;
+        while (x < 4){
+            ta[x] = new Triangle();
+            ta[x].height = (x+1) * 2;
+            ta[x].length = x + 4;
+            ta[x].setArea();
 
-        int ref;
-        while (y<4) {
-            ref = index[y];
-            System.out.print("island = ");
-            System.out.println(islands[ref]);
-            y = y + 1;
+            System.out.print("triangle " + x + ", area");
+            System.out.println(" = " + ta[x].area);
+            x = x + 1;
         }
+
+        int y = x;
+        x = 27;
+        Triangle t5 = ta[2];
+        ta[2].area = 343;
+        System.out.print("y = " + y);
+        System.out.println(", t5 area = " + t5.area);
+    }
+
+    void setArea() {
+        area = (height * length) / 2;
     }
 }
--- studied/JAVAExtra.md
+++ studied/JAVAExtra.md
@@ -198,3 +198,42 @@ public class Dog {
         System.out.println(name + " 이의 나이는 " + age +"살 입니다.");
     }
 }
+
+
+# 5. 수영장 퍼즐 p.107
+package practice;
+
+class Triangle{
+    double area;
+    int height;
+    int length;
+
+    public static void main(String[] args) {
+        int x = 0;
+        Triangle [] ta = new Triangle[4];
+
+        while (x < 4){
+            ta[x] = new Triangle();
+            ta[x].height = (x+1) * 2;
+            ta[x].length = x + 4;
+            ta[x].setArea();
+
+            System.out.print("triangle " + x + ", area");
+            System.out.println(" = " + ta[x].area);
+            x = x + 1;
+        }
+
+        int y = x;
+        x = 27;
+        Triangle t5 = ta[2];
+        ta[2].area = 343;
+        System.out.print("y = " + y);
+        System.out.println(", t5 area = " + t5.area);
+    }
+
+    void setArea() {
+        area = (height * length) / 2;
+    }
+}
+
+배열에서 ta[x].height, ta[x].length로 변수를 입력한 다음 ta[x].setArea();로 계산할 수 있다.
